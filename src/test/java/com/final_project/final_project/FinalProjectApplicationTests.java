53bb3102228244dc57aea2ef873f84069821aaa2
june <june@naver.com>
2024-04-28 15:35:00 +0900

    Initial commit

---
+++ src/test/java/com/final_project/final_project/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_project.final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
