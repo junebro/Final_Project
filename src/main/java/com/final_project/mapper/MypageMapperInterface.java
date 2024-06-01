9e415e8b21a55829e07094ad7342c7e4c128fccf
june <june@naver.com>
2024-06-02 00:26:13 +0900

    마이페이지 커뮤니티활동 했어요

--- src/main/java/com/final_project/mapper/MypageMapperInterface.java
+++ src/main/java/com/final_project/mapper/MypageMapperInterface.java
@@ -10,13 +10,18 @@ import java.util.List;
 @Mapper
 public interface MypageMapperInterface {
     // 로그인 멤버의 작성글 조회
-    @Select(value = "SELECT * FROM BOARDS b left outer join tmem m on b.mbrno = m.memno WHERE MBRNO = ${userNo};")
+    @Select(value = "SELECT \n" +
+            " A.mbrno, A.bono, A.botitle, B.membernick, A.bo_create_at, A.viewCount \n" +
+            " FROM boards A\n" +
+            " LEFT OUTER JOIN tmem B on A.mbrno = B.memno\n" +
+            " WHERE A.mbrno = ${userNo};")
     List<MypageDTO> SelectAll(@Param("userNo")String userNo);
 
     // 좋아요한 글 조회
-    @Select(value = "SELECT * FROM BOARDS b\n" +
-            "LEFT OUTER JOIN tmem m ON b.mbrno = m.memno\n" +
-            "LEFT OUTER JOIN likes l ON b.bono = l.bono\n" +
-            "WHERE b.MBRNO = ${userNo};")
+    @Select(value = "SELECT \n" +
+            " A.mbrno, A.bono, A.botitle, C.membernick, A.bo_create_at, A.viewCount FROM boards A\n" +
+            " LEFT OUTER JOIN likes B on A.bono = B.bono\n" +
+            " LEFT OUTER JOIN tmem C ON A.mbrno = C.memno\n" +
+            " WHERE B.mbrno = ${userNo};")
     List<MypageDTO> SelectLike(@Param("userNo")String userNo);
 }
